5a274e90e5b205bcea3d9e336e840305ba10dbb8
Olaolu Odushola <olaoluodushola@gmail.com>
2024-03-09 00:25:05 -0500

    implement admin category feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
+  end
+
   get 'about/index'
   root to: 'products#index'
 
@@ -16,6 +20,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
   get '/about', to: 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
